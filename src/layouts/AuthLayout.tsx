df0f986064a0e4954f8f8c63ca7c3a996d82c520
ravi <ravi.7seasol@gmail.com>
2022-01-05 14:48:41 +0530

    auth layout changes

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -18,7 +18,9 @@ const AuthLayout: FC<Props> = ({ children, ...props }) => {
     <div>
         {/* {is_loggedin ?  */}
         <AuthHeader /> 
-        {/* : <Header /> } */}
+        {/* :  */}
+        {/* <Header />  */}
+        {/* } */}
         <div className={location.pathname === "/" ? "pb-308 bg-homepage" : "pb-308"} {...props}>{children}</div>
         {/* <div className="pb-308" {...props}>{children}</div> */}
         {/* <Footer/> */}
