34717b5fe776097a83d239dc0560a37bb91fbd41
ravi <ravi.7seasol@gmail.com>
2022-01-08 16:19:30 +0530

    instagram icon issue resolved

--- src/pages/signup/LogiWithInstagram.js
+++ src/pages/signup/LogiWithInstagram.js
@@ -1,24 +1,36 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
 import InstagramLogin from 'react-instagram-login'
 const FacbookAppId = "634703847650865"
 
 const LogiWithInstagram = () => {
     const responseInstagram = (response) => {
+        debugger
         console.log("instagram response ======= response", response);
     };
 
     const failureResponseInstagram = (response) => {
+        debugger
         console.log("instagram failureResponseFacebook ===== failureResponseFacebook", response);
     };
+
+    const Insta = () => {
+        document.getElementById('insta').getElementsByTagName('button')[0].click();
+    }
+
     return (
         <div>
-            <InstagramLogin
-                clientId={FacbookAppId}
-                buttonText="Login"
-                onSuccess={responseInstagram}
-                onFailure={failureResponseInstagram}
-            />
-
+            <figure onClick={Insta}>
+                <img src="./assets/img/instagram-icon.png" alt="icon" />
+            </figure>
+            <div id="insta" >
+                <InstagramLogin
+                    clientId={FacbookAppId}
+                    buttonText="Login"
+                    onSuccess={responseInstagram}
+                    onFailure={failureResponseInstagram}
+                />
+            </div>
         </div>
     )
 }
