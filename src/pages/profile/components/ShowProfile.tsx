8dd83d5f1ec007376820c9b3f0070b7d1f2778d6
jenishpansuriya <jenishpansuriya.7seasol@gmail.com>
2022-01-13 18:56:40 +0530

    Change in lastname and relationship button

--- src/pages/profile/components/ShowProfile.tsx
+++ src/pages/profile/components/ShowProfile.tsx
@@ -14,7 +14,7 @@ import moment from 'moment';
 const ShowProfile = () => {
 
     const [getProfileData, setGetProfileData] = useState({
-        name: '',
+        firstname: '',
         dob: '',
         address: '',
         gender: '',
@@ -30,7 +30,7 @@ const ShowProfile = () => {
         workout: '',
         consume_alcohol: '',
         smoke: '',
-
+        religion: '',
         body_type: "",
         career: "",
         children: "",
@@ -53,11 +53,6 @@ const ShowProfile = () => {
         token: "",
     })
 
-    const navigate = useNavigate()
-    const handleRedirect = () => {
-        navigate("/match_or_message")
-    }
-
     useEffect(() => {
         const id = {
             id: '13'
@@ -67,7 +62,7 @@ const ShowProfile = () => {
         ApiPost(`getsingleuser`, body)
             .then((res: any) => {
                 setGetProfileData({
-                    ...getProfileData, name: res.user.firstname, dob: res.user.dob, address: res.user.address, gender: res.user.gender, denomination: res.user.denomination, your_story: res.user.your_story, short_bio: res.user.short_bio, relationship_status: res.user.relationship_status, intrusted_in_meating: res.user.intrusted_in_meating, relationship_want_to_build: res.user.relationship_want_to_build, your_intenet: res.user.your_intenet, how_often_church: res.user.how_often_church, read_bible: res.user.read_bible, workout: res.user.workout, consume_alcohol: res.user.consume_alcohol, smoke: res.user.smoke,
+                    ...getProfileData, firstname: res.user.firstname, dob: res.user.dob, address: res.user.address, gender: res.user.gender, denomination: res.user.denomination, your_story: res.user.your_story, short_bio: res.user.short_bio, relationship_status: res.user.relationship_status, intrusted_in_meating: res.user.intrusted_in_meating, relationship_want_to_build: res.user.relationship_want_to_build, your_intenet: res.user.your_intenet, how_often_church: res.user.how_often_church, read_bible: res.user.read_bible, workout: res.user.workout, consume_alcohol: res.user.consume_alcohol, smoke: res.user.smoke,
                     body_type: res.user.body_type, career: res.user.career, children: res.user.children, city: res.user.city, code: res.user.code, country: res.user.country, education: res.user.education, email: res.user.email, funfacts: res.user.funfacts, id: res.user.id, image: res.user.image, is_active: res.user.is_active, is_verify: res.user.is_verify, language: res.user.language, lastname: res.user.lastname, mobile_no: res.user.mobile_no, pets: res.user.pets, profile_picture: res.user.profile_picture, state: res.user.state, token: res.user.token
                 })
 
@@ -75,6 +70,10 @@ const ShowProfile = () => {
                 console.log(error);
             })
     }, [])
+
+    useEffect(() => {
+        console.log("getProfileData.image",getProfileData.image);
+    }, [getProfileData.image])
 
     const accordion = [
         {
@@ -135,7 +134,7 @@ const ShowProfile = () => {
                                 <img src="./assets/img/next.png" alt="" width="10px" height="15px" />
                             </Link>
                         </div>
-                        <Buttons ButtonStyle='single-btn' onClick={handleRedirect} children="Single" />
+                        <Buttons ButtonStyle='single-btn' onClick={()=>{}} children={getProfileData.relationship_status} />
                     </div>
                     <div className='over-img-div-991'>
                         <Row>
@@ -150,10 +149,10 @@ const ShowProfile = () => {
                             <Col md={9}>
                                 <div className="over-img-popup">
                                     <div className="d-flex align-items-center mb-3">
-                                        <h5 className='name-age'>{getProfileData.name}, {moment().diff(moment(getProfileData.dob, 'YYYY-MM-DD'), 'years')}</h5>
+                                        <h5 className='name-age'>{getProfileData.firstname} {getProfileData.lastname} , {moment().diff(moment(getProfileData.dob, 'YYYY-MM-DD'), 'years')}</h5>
                                         {getProfileData.gender === "male" ? <img src="./assets/img/male.png" alt="" className='ml-3' /> : <img src="./assets/img/female.png" alt="" className='ml-3' />}
                                     </div>
-                                    <p>{getProfileData.address} | Religion: <span> Catholic </span></p>
+                                    <p>{getProfileData.address} | Religion: {getProfileData.religion}</p>
                                     <p className='about-mi'>{getProfileData.short_bio}</p>
                                 </div>
                             </Col>
@@ -167,9 +166,9 @@ const ShowProfile = () => {
                         {getProfileData.is_verify === "1" ? <><img src="./assets/img/poltgon-group.png" alt="" /><p>Verified picture</p></> : ''}
                     </div>
                     <div className="over-img-popup">
-                        <p>{getProfileData.address} | Religion: <span> Catholic </span></p>
+                        <p>{getProfileData.address} | Religion: {getProfileData.religion}</p>
                         <div className="d-flex align-items-center mb-3">
-                            <h5 className='name-age'>{getProfileData.name}, {moment().diff(moment(getProfileData.dob, 'YYYY-MM-DD'), 'years')}</h5>
+                            <h5 className='name-age'>{getProfileData.firstname} {getProfileData.lastname}, {moment().diff(moment(getProfileData.dob, 'YYYY-MM-DD'), 'years')}</h5>
                             <img src="./assets/img/male.png" alt="" className='ml-3' />
                         </div>
                         <p className='about-mi'>{getProfileData.short_bio}</p>
