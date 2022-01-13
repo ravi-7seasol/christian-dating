c7633170a2ba3c7cc7a9d0a80b07c644633c4178
bhargav <bhargavdiyora3738@gmail.com>
2022-01-13 10:04:49 +0530

    forgot password page

--- src/pages/forgotpassword/ForgotPassword.tsx
+++ src/pages/forgotpassword/ForgotPassword.tsx
@@ -1,18 +1,43 @@
-import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import { faCheckCircle, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Buttons from "../../components/Buttons";
 import InputField from "../../components/Inputfield";
+import { ApiPost } from "../../helper/API/ApiData";
+import { xwwwFormUrlencoded } from "../../helper/utils";
 import Header from "../../layouts/header/Header";
 
 const ForgotPassword = () => {
+
+  const [emailData, setEmailData] = useState({
+    email: "",
+  });
+  const [successMsg, setSuccessMsg] = useState("");
+
+  const onInputValueChange = (e: any) => {
+    setEmailData({
+      ...emailData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const forgotPassword = () => {
+    const body = xwwwFormUrlencoded(emailData);
+    ApiPost('forgotpassword', body)
+      .then((res: any) => {
+        setSuccessMsg(res.msg);
+      }).catch((error) => {
+        console.log(error);
+
+      })
+  }
   return (
     <>
       <div
         className="d-flex justify-content-center align-items-center main-page"
-        style={{ height: "100vh" }} 
+        style={{ height: "100vh" }}
       >
         <Container>
           <div className="login-card">
@@ -23,26 +48,27 @@ const ForgotPassword = () => {
                 <InputField
                   name="email"
                   maxLength={undefined}
-                  value=""
+                  value={emailData.email}
                   lablestyleClass="login-label"
                   InputstyleClass="login-input"
-                  onChange={(e: any) => {}}
+                  onChange={(e: any) => { onInputValueChange(e) }}
                   disabled={false}
                   label="Email address"
                   placeholder="email@example.com"
                   type="email"
                   fromrowStyleclass=""
                 />
-                <label className="ErrMsg" htmlFor="error">
-                  {" "}
-                  <FontAwesomeIcon icon={faTimesCircle} /> Email or password
-                  wrong
-                </label>
+
+                <br />
+                {
+                  successMsg ? < label className="SuccessMsg"><FontAwesomeIcon icon={faCheckCircle} /> {successMsg}</label> : null
+                }
+
 
                 <div style={{ marginTop: "9rem" }}>
                   <Buttons
                     children="Log in"
-                    onClick={() => {}}
+                    onClick={() => { forgotPassword() }}
                     ButtonStyle="login-btn"
                     disabled={false}
                   />
