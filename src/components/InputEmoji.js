779b032528aa6557f96e8ca94244e323e67a6b28
ravi <ravi.7seasol@gmail.com>
2022-01-13 19:56:40 +0530

    chnages

--- src/components/InputEmoji.js
+++ src/components/InputEmoji.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import InputEmoji from "react-input-emoji";
 
-const InpEmoji = () => {
+const InpEmoji = ({getMData}) => {
   const [text, setText] = useState("");
   function handleOnEnter (text) {
     console.log('enter', text)
